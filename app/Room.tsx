fad092594bc6a066299f5a1789966fa248b2ca97
Mustafa KILIC <klcmustafaa@gmail.com>
2024-09-12 22:52:02 +0300

    Created Figma Clone Project

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_4U96M40jJE9vt7O9mNnLBxoCunS4UWM3LsuOwvpJ6D3W73cfTP1KRS87ImWo0leY"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
